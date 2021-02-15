6b0ee1a401da310afc21f915d2cd1f3c08057d1b
Allixor <44477549+Allixor@users.noreply.github.com>
2021-02-15 12:07:17 -0500

    App from Alex
    Replaced files

---
+++ DonationAppFiles/app/src/main/java/com/example/a4p02app/profile.java
@@ -0,0 +1,30 @@
+package com.example.a4p02app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+    public void goHome(View view) {
+    }
+
+    public void goBack(View view) {
+    }
+
+    public void goProfile(View view) {
+    }
+
+    public void goFavs(View view) {
+    }
+
+    public void goDonos(View view) {
+    }
+}
