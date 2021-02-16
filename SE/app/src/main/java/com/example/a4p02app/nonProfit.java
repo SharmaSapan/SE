307b5c9d51e2e47f159fa7395e2fc9c68e7da830
Alanna McNulty <51451767+am17xy@users.noreply.github.com>
2021-02-16 14:44:45 -0500

    Update nonProfit.java
    updated to display a test non-profit organization

--- SE/app/src/main/java/com/example/a4p02app/nonProfit.java
+++ SE/app/src/main/java/com/example/a4p02app/nonProfit.java
@@ -2,18 +2,63 @@ package com.example.a4p02app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+//the default profile picture is from https://pixabay.com/vectors/blank-profile-picture-mystery-man-973460/
 
 public class nonProfit extends AppCompatActivity {
+
+    private String npName;
+    private String npDesc;
+    private Image profilePic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_non_profit);
+
+        //this page should be sent the name of the non-profit in the intent so that
+        //the correct non-profit is displayed
+        Bundle extras = getIntent().getExtras();
+        if (extras == null) {
+            //if there is no nonprofit name given in the intent display a default one
+            npName = "Test Non-Profit";
+
+            npDesc = "Test non-profit description";
+        } else {
+            //get all info from the database here
+            npName = extras.getString("NON_PROFIT_TO_DISPLAY");
+        }
+
+        //update the page (will need to add test nonprofits to the database, for now am just testing with hard-coded strings)
+        getInfo();
     }
 
-   public void goBack(View view) {
+    //this method updates the page to match the non-profit's info
+    //needs to be updated to use info from the database
+    public void getInfo(){
+        //update non-profit's name
+        TextView nonProfitName = (TextView) findViewById(R.id.npNameDisplay);
+        nonProfitName.setText(npName);
+
+        //update profile picture
+        //this part needs to be updated to match the one from the database
+        ImageView nonProfitPic = (ImageView) findViewById(R.id.profilePic);
+        nonProfitPic.setImageResource(R.drawable.blank_profile_picture);
+
+        //update description
+        TextView nonProfitDescription = (TextView) findViewById(R.id.profilePart);
+        nonProfitDescription.setText(npDesc);
+    }
+
+    public void goBack(View view) {
         //go back to the previous activity
         finish();
     }
