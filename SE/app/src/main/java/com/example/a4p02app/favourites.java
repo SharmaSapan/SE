7b5064de60ae686a1c5c25ef4dc601a459954e35
Alexander-Bowles <68672786+Alexander-Bowles@users.noreply.github.com>
2021-03-02 15:25:53 -0500

    Update favourites

--- SE/app/src/main/java/com/example/a4p02app/favourites.java
+++ SE/app/src/main/java/com/example/a4p02app/favourites.java
@@ -6,80 +6,46 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.TranslateAnimation;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class favourites extends AppCompatActivity {
 
-    boolean searcherIsDown;
-    View slidingSearch;
-    View searcher;
-    View closerArrow;
-    View header;
-    View headerline;
-    View sb;
-    View standin;
-    View searchbar;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //initialize favourites page
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        slidingSearch = findViewById(R.id.searcher);
-        slidingSearch.setVisibility(View.INVISIBLE);
-        searcherIsDown = false;
+        setContentView(R.layout.activity_favourites);
     }
 
-    private void slideUp() {
-        //method to bring up the searchbar
+    public void getPostsFromDatabase () {
+        DocumentReference docRef = db.collection("cities").document("SF");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
 
-        header = findViewById(R.id.header); //sets the header to be visible
-        header.setVisibility(View.VISIBLE);
-        searcher = findViewById(R.id.searcher);//sets the entire search area to be visible
-        searcher.setVisibility(View.VISIBLE);
-        headerline.setElevation(1);   //this is to under impose searcher onto what is in background
-        TranslateAnimation animate = new TranslateAnimation(0, 0, 0, slidingSearch.getHeight()*-1);
-        animate.setDuration(0);
-        animate.setFillAfter(true);
-        sb = findViewById(R.id.search_button); //sets the search button to be visible
-        sb.setVisibility(View.VISIBLE);
-        slidingSearch.startAnimation(animate);
-        slidingSearch.setVisibility(View.GONE); //sets the search area to be gone
-
-        standin = findViewById(R.id.standin); //to be replaced by search bar
-        standin.setVisibility(View.GONE);
-        closerArrow = findViewById(R.id.closerArrow); //sets the closer arrow to be gone
-        closerArrow.setVisibility(View.GONE);
-
-        searcherIsDown = false;
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
     }
 
-    public void reveal(View view) {
-        if (searcherIsDown) {
-            slideUp();
+    public void getInfo() {
 
-        }
-        else {
-            header = findViewById(R.id.header); //sets the header to be visible
-            header.setVisibility(View.VISIBLE);
-            headerline = findViewById(R.id.headerline); //sets the header border to be gone
-            headerline.setVisibility(View.GONE);
-            standin = findViewById(R.id.standin); //to be replaced with search bar
-            standin.setVisibility(View.GONE);
-            TranslateAnimation animate = new TranslateAnimation(0,0, slidingSearch.getHeight()*-1,0);
-            animate.setDuration(500);
-            animate.setFillAfter(true);
-            slidingSearch.startAnimation(animate);
-            slidingSearch.postDelayed(() -> standin.setVisibility(View.VISIBLE), 300);
-            //delays start of Visibility of standin/searchbar
-            sb = findViewById(R.id.search_button); //sets the search button to be gone
-            sb.setVisibility(View.GONE);
-            closerArrow = findViewById(R.id.closerArrow); //sets the closer arrow to be visible
-            closerArrow.setVisibility(View.VISIBLE);
-
-
-            searcherIsDown = true;
-        }
     }
 
     public void goDonos(View view) {
