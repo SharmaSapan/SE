fe2d74825d22f6b1f7bc4ab6285d96a00a9efe51
Alanna McNulty <51451767+am17xy@users.noreply.github.com>
2021-03-01 23:31:23 -0500

    Update nonProfit.java

--- SE/app/src/main/java/com/example/a4p02app/nonProfit.java
+++ SE/app/src/main/java/com/example/a4p02app/nonProfit.java
@@ -1,13 +1,21 @@
 package com.example.a4p02app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.w3c.dom.Text;
 
@@ -18,6 +26,10 @@ public class nonProfit extends AppCompatActivity {
     private String npName;
     private String npDesc;
     private int profilePic;
+    private String profilePicName;
+
+    //get an instance of Firebase so that the firestore database can be used
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,22 +39,49 @@ public class nonProfit extends AppCompatActivity {
         //this page should be sent the name of the non-profit in the intent so that
         //the correct non-profit is displayed
         Bundle extras = getIntent().getExtras();
-        if (extras == null) {
+       // if (extras == null) {
             //if there is no nonprofit name given in the intent display a default one
-            npName = "Test Non-Profit";
-            profilePic = R.drawable.blank_profile_picture;
-            npDesc = "Test non-profit description";
-        } else {
+       //     npName = "Test Non-Profit";
+       //     profilePic = R.drawable.blank_profile_picture;
+       //     npDesc = "Test non-profit description";
+       // } else {
             //get all info from the database here
-            npName = extras.getString("NON_PROFIT_TO_DISPLAY");
-        }
+            //npName = extras.getString("NON_PROFIT_TO_DISPLAY");
+
+            getFromDatabase();
+       // }
 
         //update the page (will need to add test nonprofits to the database, for now am just testing with hard-coded strings)
         getInfo();
     }
 
+    //this method gets the info from the database
+    //not done yet
+    //for more info on this, go to https://firebase.google.com/docs/firestore/query-data/get-data
+    public void getFromDatabase() {
+        //should change testNP later
+        DocumentReference docRef = db.collection("nonprofits").document("cYHsj179NpLh2Hp6evoNcYHsj179NpLh2Hp6evoN");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d("TAG: ", "DocumentSnapshot data: " + document.getData());
+                        npName = (String) document.getString("npName");
+                        npDesc = (String) document.getString("npDesc");
+                        profilePicName = document.getString("profilePic");
+                    } else {
+                        Log.d("TAG: ", "No such document");
+                    }
+                } else {
+                    Log.d("TAG: ", "get failed with ", task.getException());
+                }
+            }
+        });
+    }
+
     //this method updates the page to match the non-profit's info
-    //needs to be updated to use info from the database
     public void getInfo(){
         //update non-profit's name
         TextView nonProfitName = (TextView) findViewById(R.id.npNameDisplay);
