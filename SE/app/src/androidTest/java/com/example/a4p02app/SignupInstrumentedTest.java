c319eeb269e30fb8f5a4e2e2e19f76eeef847197
Brian <bj16hh@brocku.ca>
2021-04-28 17:06:56 -0400

    more instrumental tests, unit tests next

--- SE/app/src/androidTest/java/com/example/a4p02app/SignupInstrumentedTest.java
+++ SE/app/src/androidTest/java/com/example/a4p02app/SignupInstrumentedTest.java
@@ -9,45 +9,53 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Random;
+
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isNotChecked;
+import static androidx.test.espresso.matcher.ViewMatchers.isSelected;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class LoginTest {
+public class SignupInstrumentedTest {
 
+    private Random rand;
     private String testPass;
     private String testEmail;
+    private String testConfirm;
 
     @Rule
-    public ActivityScenarioRule<LoginActivity> loginRule = new ActivityScenarioRule<>(LoginActivity.class);
+    public ActivityScenarioRule<SignUpActivity> signupRule = new ActivityScenarioRule<>(SignUpActivity.class);
 
 
     @Before
     public void initValidString() {
-        testEmail = "bj16hh@brocku.ca";
+        rand = new Random();
+        int r = rand.nextInt((9999 - 100) + 1) + 10;
+        testEmail = "user_" + Integer.toString(r) + "@test.junit";
         testPass = "asdf1234";
+        testConfirm = testPass;
     }
 
     @Test
-    public void testLogin() {
-        ActivityScenarioRule<MainActivity> mainRule = new ActivityScenarioRule<>(MainActivity.class);
-
-        // Type text and then press the button.
+    public void testSignUp() {
         onView(withId(R.id.txtEmail)).perform(typeText(testEmail), closeSoftKeyboard());
         onView(withId(R.id.txtPassword)).perform(typeText(testPass), closeSoftKeyboard());
+        onView(withId(R.id.txtConfirm)).perform(typeText(testConfirm), closeSoftKeyboard());
+        onView(withId(R.id.rdoBusiness)).perform(click());
 
-
-        // Check that the text was changed.
         onView(withId(R.id.txtPassword)).check(matches(withText(testPass)));
         onView(withId(R.id.txtEmail)).check(matches(withText(testEmail)));
+        onView(withId(R.id.txtConfirm)).check(matches(withText(testPass)));
+        onView(withId(R.id.rdoPersonal)).check(matches(isNotChecked()));
 
-        onView(withId(R.id.btnLogIn)).perform(click());
+        onView(withId(R.id.btnSignUp)).perform(click());
     }
 }
---
+++ SE/app/src/main/java/com/example/a4p02app/interactors/LoginInteractor.java
@@ -0,0 +1,7 @@
+package com.example.a4p02app.interactors;
+
+public class LoginInteractor {
+
+
+
+}
